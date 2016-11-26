86e52295e4b0ab0555e00b42444319c54205d3b0
mikhail.kutuzov <mkutuzov@kostroma.ru>
2016-11-26 16:16:11 +0300

    именно для создания и загрузки chrome driver

---
+++ src/main/java/test/smoketest/utils/ProduceChromeDriver.java
@@ -0,0 +1,21 @@
+package test.smoketest.utils;
+
+import java.io.File;
+
+/**
+ * Загрузим ссылку на созданный файл в переменную webdriver.chrome.driver.
+ * Created by mikhail.kutuzov on 26.11.2016.
+ */
+public class ProduceChromeDriver implements FileFromResource {
+    FileFromResource producer;
+
+    public ProduceChromeDriver(FileFromResource producer) {
+        this.producer = producer;
+    }
+
+    @Override
+    public void create(String resource, File path) {
+        producer.create(resource, path);
+        System.setProperty("webdriver.chrome.driver", path.getAbsolutePath());
+    }
+}
