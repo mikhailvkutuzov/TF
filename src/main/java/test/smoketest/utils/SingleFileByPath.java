f19814cf818db0cbd6bf4f281eeeab07b7854866
mikhail.kutuzov <mkutuzov@kostroma.ru>
2017-06-26 09:47:14 +0300

    * Linux Chrome driver has been added into resources * SingleFileByPath.java is setting POSIX file permissions * eliminate some compilation warnings

--- src/main/java/test/smoketest/utils/SingleFileByPath.java
+++ src/main/java/test/smoketest/utils/SingleFileByPath.java
@@ -1,18 +1,25 @@
 package test.smoketest.utils;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.attribute.PosixFilePermission;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
+ * This class intended to create file from a single project resource.
+ * It makes a file executable and readable by anyone according with POSIX.
+ * <p>
  * Created by mikhail.kutuzov on 26.11.2016.
  */
 public class SingleFileByPath implements FileFromResource {
 
     @Override
     public void create(String resource, File path) {
-        BufferedInputStream input =  new BufferedInputStream(SingleFileByPath.class.getClassLoader().getResourceAsStream(resource));
+        BufferedInputStream input = new BufferedInputStream(SingleFileByPath.class.getClassLoader().getResourceAsStream(resource));
         BufferedOutputStream output = null;
         try {
-            output =  new BufferedOutputStream(new FileOutputStream(path));
+            output = new BufferedOutputStream(new FileOutputStream(path));
             int i;
             while ((i = input.read()) != -1) {
                 try {
@@ -21,6 +28,12 @@ public class SingleFileByPath implements FileFromResource {
                     throw new WritingError();
                 }
             }
+            Set<PosixFilePermission> permissions = new HashSet<>();
+            permissions.add(PosixFilePermission.OWNER_READ);
+            permissions.add(PosixFilePermission.OWNER_EXECUTE);
+            permissions.add(PosixFilePermission.OTHERS_READ);
+            permissions.add(PosixFilePermission.OTHERS_EXECUTE);
+            Files.setPosixFilePermissions(path.toPath(), permissions);
         } catch (FileNotFoundException e) {
             throw new UnknownError();
         } catch (IOException e) {
@@ -33,7 +46,9 @@ public class SingleFileByPath implements FileFromResource {
                 throw new CouldNotClose();
             } finally {
                 try {
-                    output.close();
+                    if (output != null) {
+                        output.close();
+                    }
                 } catch (IOException e) {
                     throw new CouldNotClose();
                 }
@@ -41,16 +56,19 @@ public class SingleFileByPath implements FileFromResource {
         }
     }
 
-    public static final class WritingError extends RuntimeException {}
+    private static final class WritingError extends RuntimeException {
+    }
 
-    public static final class ReadingError extends RuntimeException {
-        public ReadingError(Throwable cause) {
+    private static final class ReadingError extends RuntimeException {
+        ReadingError(Throwable cause) {
             super(cause);
         }
     }
 
-    public static final class CouldNotClose extends RuntimeException {}
+    private static final class CouldNotClose extends RuntimeException {
+    }
 
-    public static final class UnknownError extends RuntimeException {}
+    private static final class UnknownError extends RuntimeException {
+    }
 
 }
